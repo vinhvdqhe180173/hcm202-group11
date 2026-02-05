74754d0d7726cb926c77df365f6b66b7d70f561a
Vinh Vũ <vinhvu@Vinhs-MacBook-Air.local>
2026-02-05 12:28:32 +0700

    fix: not scroll to the top when switch page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -8,9 +9,20 @@ import PartyPage from './pages/PartyPage';
 import StatePage from './pages/StatePage';
 import LegacyPage from './pages/LegacyPage';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">
